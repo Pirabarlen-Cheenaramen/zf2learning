0585ee6699faeae07a14d16c212025fb68cad0d8
Pirabarlen-Cheenaramen <selven@hackers.mu>
2016-06-05 11:24:09 +0200

    typos

--- module/ProjectTimer/src/ProjectTimer/Model/ProjectTimer.php
+++ module/ProjectTimer/src/ProjectTimer/Model/ProjectTimer.php
@@ -3,14 +3,14 @@ namespace ProjectTimer\Model;
 
 class ProjectTimer
 {
-    public $project_id
+    public $project_id;
     public $start_time;
     public $stop_time;
 
     public function exchangeArray($data)
     {
         $this->project_id=(!empty($data['project_id'])) ? $data['project_id'] : null;
-        $this->start_time=(!empty($data['start_time'])) ? $data['start_time] : null;
+        $this->start_time=(!empty($data['start_time'])) ? $data['start_time'] : null;
         $this->stop_time=(!empty($data['stop_time'])) ? $data['stop_time'] : null;
     }
 }
