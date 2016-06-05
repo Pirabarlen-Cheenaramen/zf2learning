d20c12349a689385982c356239cf8a80eba1f1ce
Pirabarlen-Cheenaramen <selven@hackers.mu>
2016-06-05 19:20:21 +0200

    Form to add projects

---
+++ module/ProjectTimer/src/ProjectTimer/Form/ProjectTimerForm.php
@@ -0,0 +1,34 @@
+<?php
+ namespace ProjectTimer\Form;
+
+ use Zend\Form\Form;
+
+ class ProjectTimerForm extends Form
+ {
+     public function __construct($name = null)
+     {
+         // we want to ignore the name passed
+         parent::__construct('projecttimer');
+
+         $this->add(array(
+             'name' => 'id',
+             'type' => 'Hidden',
+         ));
+         $this->add(array(
+             'name' => 'projectname',
+             'type' => 'Text',
+             'options' => array(
+                 'label' => 'Title',
+             ),
+         ));
+         $this->add(array(
+             'name' => 'submit',
+             'type' => 'Submit',
+             'attributes' => array(
+                 'value' => 'Go',
+                 'id' => 'submitbutton',
+             ),
+         ));
+     }
+ }
+?>
